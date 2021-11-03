df9c0ff5d9890535f53a38ad0049f6f703d89680
cangozpinar <cgozpinar18@ku.edu.tr>
2021-11-03 03:04:06 +0300

    modified dev branch to download all the available screenshots instead of just a couple.

--- data colleciton pipeline/app.js
+++ data colleciton pipeline/app.js
@@ -36,9 +36,9 @@ takeScreenshot = async (url, dir) => {
 
 const webPageUrls = require("./resources/webPageUrls.json").webPageUrls;
 takeScreenshots = (webPageUrls, dir, pageName) => {
-    //TODO: uncomment the line below and comment the line that is two lines below when you want to download all of the screenshots!
-    //for(let i = 0; i < webPageUrls.length; i++){
-    for(let i = 0; i < 5; i++){
+    //TODO: comment the line below and uncomment the line that is two lines below when you want to download only 5 screenshots instead off all ~5k !
+    for(let i = 0; i < webPageUrls.length; i++){
+    //for(let i = 0; i < 5; i++){
         current_path = path.join(dir, pageName + '-' + i.toString() + '.png');
         current_url = webPageUrls[i]
         takeScreenshot(current_url, current_path)  
