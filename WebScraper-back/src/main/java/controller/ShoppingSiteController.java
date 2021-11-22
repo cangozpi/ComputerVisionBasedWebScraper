c0b53f9f27ea557876aea17bbc82cef801881587
cangozpinar <cgozpinar18@ku.edu.tr>
2021-11-22 21:33:09 +0300

    @fix:fixed controller not working bug in spring boot -can

--- WebScraper-back/src/main/java/controller/ShoppingSiteController.java
+++ WebScraper-back/src/main/java/controller/ShoppingSiteController.java
@@ -8,8 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("scrape/shoppingSite")
 public class ShoppingSiteController {
 
