4165637ec9253d2161468db7a5bffc94144e9516
cangozpinar <cgozpinar18@ku.edu.tr>
2021-11-22 21:14:33 +0300

    added Form value extraction upon form submission into ts -can

--- webScraper-front/src/app/components/input-form/input-form.component.ts
+++ webScraper-front/src/app/components/input-form/input-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input-form',
@@ -7,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputFormComponent implements OnInit {
 
+  // Form handling
+  inputForm = new FormGroup({
+    websiteType: new FormControl(''),
+    targetURL: new FormControl(''),
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.inputForm.value);
+  }
+
+
+  // value is for clearing URL button
   value = '';
-  
+  //
+
   constructor() { }
 
   ngOnInit(): void {
