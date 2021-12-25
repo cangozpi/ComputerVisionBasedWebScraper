1839dcf75fe6e392ebd818d6c4d6387c228507fb
Berkut <bgokmen18@ku.edu.tr>
2021-12-25 15:18:06 +0300

    Finished data export script.

--- ExternalServices/dataExport.js
+++ ExternalServices/dataExport.js
@@ -5,12 +5,12 @@ const imageFolder = './DataScraper/images/';
 const ocrFolder = './OCR/OCR_output/';
 var args = process.argv.slice(2);
 let siteType = args[0]
-
+let jsonTemplate = {}
 
 
 switch (siteType) {
     case 'shopping-site':
-        let jsonTemplate = {
+        jsonTemplate = {
             title: null,
             seller: null,
             ratings: null,
@@ -23,17 +23,32 @@ switch (siteType) {
             summary: null,
             product_desc: null
         }
-        populateJSON(jsonTemplate);
         break;
     case 'forum-site':
+        jsonTemplate = {
+            main_topic: null,
+            main_post: null,
+            post_owner: null,
+            date_info: null,
+            forum_category: null,
+            data: null
+        }
         break;
     case 'news-site':
+        jsonTemplate = {
+            main_topic: null,
+            main_post: null,
+            post_owner: null,
+            date_info: null,
+            forum_category: null,
+            main_text: null
+        }
         break;
     default:
         console.log("siteType does not exist.[DataExport.js]");
 
 }
-exit()
+populateJSON(jsonTemplate);
 
 // function to encode file data to base64 encoded string
 function base64_encode(file) {
@@ -47,9 +62,7 @@ async function populateJSON(jsonTemplate) {
     try {
         files = fs.readdirSync(imageFolder);
             files.forEach(function (file) {
-                if (!file.includes("review")) {
-                    jsonTemplate[file.slice(0, -5)] = base64_encode(imageFolder + file);
-                } 
+                jsonTemplate[file.slice(0, -5)] = base64_encode(imageFolder + file); 
             });
 
             fs.writeFileSync('data.json', JSON.stringify(jsonTemplate));
@@ -60,16 +73,31 @@ async function populateJSON(jsonTemplate) {
     try {
         files = fs.readdirSync(ocrFolder);
         reviews = []
+        answers = []
+        owners = []
         files.forEach(function (file) {
             // Do whatever you want to do with the file
             var ocr = fs.readFileSync(ocrFolder + file, { encoding: 'utf8', flag: 'r' });
             if (file.includes("review")) {
                 reviews.push(ocr)
+            }else if(file.includes("answer")){
+                if(file.includes("answer_owner")){
+                    owners.push(ocr)
+                }else answers.push(ocr)
             } else {
                 jsonTemplate[file.slice(0, -4)] = ocr
             }
         });
         if (siteType === "shopping-site") jsonTemplate["reviews"] = reviews
+        
+        if(siteType === "forum-site"){
+            let data = []
+            for( i in answers){
+                if(owners.length<=i)owner.push(null)
+                data.push({"answer_owner":owners[i], "answer" : answers[i]})
+            }
+            jsonTemplate["data"] = data
+        }
         fs.writeFileSync("data.json", JSON.stringify(jsonTemplate))
         console.log("data.json created.")
     } catch (err) {
